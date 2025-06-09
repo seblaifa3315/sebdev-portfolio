15634d89aafcf9ed01471733d43dec143cdb9ce1
Seb <seblaifa@hotmail.com>
2025-06-09 09:40:13 -0700

    Fixed 'View Projects' Button from Home section

--- src/components/Sections/Home.jsx
+++ src/components/Sections/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             </p>
 
             <div className="flex justify-center space-x-4">
-                <a href="#project" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
+                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
                     View Projects
                 </a>
 
